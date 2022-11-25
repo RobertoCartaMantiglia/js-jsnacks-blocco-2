1165688f9d9f9aa9ce555f1d6f60e25e0051854e
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-25 16:52:21 +0100

    ex 3

--- SNACK3/js/script.js
+++ SNACK3/js/script.js
@@ -5,9 +5,15 @@
 const numbers = [1, 2, 3, 6, 17, 22, 40, 71, 65, 78];
 
 let somma = 0;
+let media = 0;
 
 for (let i = 0; i<10; i++){
     somma += numbers[i];
-    
+
+    media = somma % 10; 
 }
+
 console.log(somma);
+console.log("media " + media);
+
+// Calcola la somma e la media dei primi 10 numeri di un array.
