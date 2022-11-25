7b215cee9c5667a299b63b0836c328cad517ce43
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-25 15:43:26 +0100

    console.log

---
+++ SNACK1/js/script.js
@@ -0,0 +1,24 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all'utente di inserire un numero,
+// se è dispari inseriscilo nell'array.
+
+
+
+const array = [];
+
+
+
+
+for (let i=0; i < 6; i++) {
+
+    const userNumbers = parseInt(prompt("inserisci un numero"));
+    console.log(userNumbers);
+    
+    let numeroDispari = false;
+
+    if (userNumbers % 2 === 1){
+        numeroDispari = true;
+    }
+}
+
+console.log(array);
