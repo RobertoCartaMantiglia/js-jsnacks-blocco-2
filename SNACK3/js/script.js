0d1d6917756e6dd9dd84d92078db47f351d01337
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-25 18:06:32 +0100

    ex 3c spiegato con i commenti

--- SNACK3/js/script.js
+++ SNACK3/js/script.js
@@ -6,16 +6,20 @@ const numbers = [1, 2, 3, 6, 17, 22, 40, 78, 71, 65,];
 
 let somma = 0;
 let media = 0;
+//maxValue è definito inizialmente come la prima variabile presente nell'array alla poszione 0
 let maxValue = numbers[0];
 
 for (let i = 0; i<10; i++){
     somma += numbers[i];
-
-    if (numbers[i] > maxValue) {
+// confronto il max value con gli elementi del array e vedo se di volta in volta c'è un valore che è maggiore di lui,
+// una volta che max value non sarà più minore rispetto ad un altro elemento, sarò sicuro che max value corrisponderà al valore più grande del array
+    if (maxValue < numbers[i]) {
         maxValue = numbers [i];
     }
 }
 
+
+// Calcola la somma e la media dei primi 10 numeri di un array.
 media = somma / 10;
 
 
@@ -23,7 +27,7 @@ console.log(somma);
 console.log("media " + media);
 console.log(maxValue);
 
-// Calcola la somma e la media dei primi 10 numeri di un array.
+
 
 
 // Calcola la somma, la media e il valore massimo dei primi 10 numeri di un array.
