98a85db7b0b8493664f49c58d157f9588586027b
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-09 17:16:48 +0100

    remake ex

--- SNACK3/js/script.js
+++ SNACK3/js/script.js
@@ -1,33 +1,29 @@
 // Calcola la somma dei primi 10 numeri di un array.
 
-
-
-const numbers = [1, 2, 3, 6, 17, 22, 40, 78, 71, 65,];
+const array = [1,2,3,4,5,6,25,79,91,30];
 
 let somma = 0;
-let media = 0;
-//maxValue è definito inizialmente come la prima variabile presente nell'array alla poszione 0
-let maxValue = numbers[0];
 
-for (let i = 0; i<10; i++){
-    somma += numbers[i];
-// confronto il max value con gli elementi del array e vedo se di volta in volta c'è un valore che è maggiore di lui,
-// una volta che max value non sarà più minore rispetto ad un altro elemento, sarò sicuro che max value corrisponderà al valore più grande del array
-    if (maxValue < numbers[i]) {
-        maxValue = numbers [i];
-    }
+for (let i=0; i<array.length; i++){
+    somma += array[i];
 }
-
+console.log(somma);
 
 // Calcola la somma e la media dei primi 10 numeri di un array.
-media = somma / 10;
+let media = somma / (array.length -1);
+console.log("la media è " + media);
 
+//maxValue è definito inizialmente come la prima variabile presente nell'array alla poszione 0
 
-console.log(somma);
-console.log("media " + media);
-console.log(maxValue);
+// confronto il max value con gli elementi del array e vedo se di volta in volta c'è un valore che è maggiore di lui,
+// una volta che max value non sarà più minore rispetto ad un altro elemento, sarò sicuro che max value corrisponderà al valore più grande del array
+// Calcola il valore massimo dei primi 10 numeri di un array.
+let maxValue = 0;
 
-
-
-
-// Calcola la somma, la media e il valore massimo dei primi 10 numeri di un array.
+for(let i=0; i<array.length; i++){
+    if(maxValue < array[i]){
+        maxValue = array[i];
+    }else{
+        console.log(maxValue);
+    }
+}
